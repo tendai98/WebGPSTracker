96af539ca3d7c4d5e36a36744aea36fe198deeae
Tendai Bhebhe <tendaibhebhe2018@gmail.com>
2024-03-14 01:45:52 +0200

    fix login

--- public/js/misc.js
+++ public/js/misc.js
@@ -11,6 +11,7 @@ function startLoginProcess() {
     $.get(`${window.location.href}auth?email=${user}&password=${pass}`, (data, status) => {
         if (status === "success") {
             if (data.code === 200) {
+                sessionStorage.setItem("email", user)
                 window.location.href = "/app.html"
             } else if (data.code === 403) {
                 alert(data.message);
@@ -21,4 +22,14 @@ function startLoginProcess() {
     }).fail(() => {
         alert("Failed to communicate with the server.");
     });
+}
+
+function checkLoginStatus(){
+    function usernameCheck(){
+        if(!(sessionStorage["email"])){
+            window.location.href = "/"
+        }
+    }   
+
+    setInterval(usernameCheck, 100)
 }
